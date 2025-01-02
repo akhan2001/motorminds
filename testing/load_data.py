4f92d3919c505c17919ee44510acef427fa795d7
akhan2001 <m.abdullah.khan567@gmail.com>
2025-01-02 00:09:12 -0500

    Adding Loading script

---
+++ testing/load_data.py
@@ -0,0 +1,13 @@
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import os
+import json
+
+STORAGE_CONNECTION_STRING = ""
+CONTAINER_NAME = "scraping-scripts"
+
+def load_data(file_path):
+
+
+if __name__ == "__main__":
+    
