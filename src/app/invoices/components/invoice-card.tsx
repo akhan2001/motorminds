41cd6c51d5d356493a4bec04ec6c14df431863b4
akhan2001 <m.abdullah.khan567@gmail.com>
2025-03-21 03:04:13 -0400

    invoice date filtering fix

--- src/app/invoices/components/invoice-card.tsx
+++ src/app/invoices/components/invoice-card.tsx
@@ -54,7 +54,7 @@ export function InvoiceCard(props: InvoiceCardProps) {
                 <div className="flex justify-between items-start">
                     <div>
                         <h3 className="text-xl font-bold flex items-center gap-2 text-white">
-                            Invoice # {props.displayNumber} <span className={`${props.status === 'PAID' ? 'text-green-500' : 'text-red-500'}`}>({props.status})</span>
+                            Invoice # {props.displayNumber || props.invoiceNumber} <span className={`${props.status === 'PAID' ? 'text-green-500' : 'text-red-500'}`}>({props.status})</span>
                         </h3>
                         <p className="text-gray-400">{props.workOrder ? props.workOrder : 'N/A'}</p>
                     </div>
