145b7d031f1b6d34e024b472de3c5692add86808
Abdullah K <m.abdullah.khan567@gmail.com>
2025-10-31 14:08:31 -0400

    invoice navbar fix

--- src/data/configs/navigation.ts
+++ src/data/configs/navigation.ts
@@ -14,7 +14,6 @@ const mechanicHubSubItems = [
     { name: "Parts & Ordering", href: "/parts-ordering" },
     { name: "Appointments", href: "/appointments" },
     { name: "Services & Parts", href: "/mechanic-hub/service-parts" },
-    { name: "Invoices", href: "/financials/invoices" },
 ];
 
 const customerSubItems = [
@@ -23,6 +22,11 @@ const customerSubItems = [
     { name: "Customer Intake Form", href: "/customer-intake" },
     { name: "Customer Invoice Intake", href: "/customer-invoice-intake" },
     { name: "Customer Contracts", href: "/customer-contracts" },
+];
+
+const financialsSubItems = [
+    { name: "Analytics", href: "/financials" },
+    { name: "Invoices", href: "/financials/invoices" },
 ];
 
 export const navigationConfig: NavItem[] = [
@@ -51,11 +55,6 @@ export const navigationConfig: NavItem[] = [
         href: "/mia-ai",
         requiredRoles: ['admin', 'super', 'demo', 'user']
     },
-    {
-        name: "Financials",
-        href: "/financials",
-        requiredRoles: ['admin', 'super', 'user']
-    },
     // {
     //     name: "Parts",
     //     href: "/parts",
@@ -73,18 +72,18 @@ export const navigationConfig: NavItem[] = [
     //     href: "/suppliers",
     //     requiredRoles: ['user', 'admin', 'super', 'demo']
     // },
-    {
-        name: "Mechanics Hub",
-        href: "/mechanic-hub",
-        hasDropdown: true,
-        subItems: mechanicHubSubItems,
-        requiredRoles: ['admin', 'super', 'user']
-    },
     // {
     //     name: "Migrations",
     //     href: "/migrations",
     //     requiredRoles: ['admin', 'super', 'user']
     // },
+    {
+        name: "Financials",
+        href: "/financials",
+        hasDropdown: true,
+        subItems: financialsSubItems,
+        requiredRoles: ['admin', 'super', 'user']
+    },
     {
         name: "Admin",
         href: "/admin",
